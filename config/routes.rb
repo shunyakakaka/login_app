c98a3939d0dbd7bb2f8878b32ff76f920819d8e4
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2022-07-15 10:23:07 +0900

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
+  fkhfaljkfslkdfjslkf
+  gsfgsfdgsdfgsdfg
+  sdfgsdfgsdfgsdfgs
+  sdfgsdfgsdfgsdfgsd
+  gsgsdfgsfgsfdgsfg
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
